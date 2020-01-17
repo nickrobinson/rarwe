936d46dd6f64d81008af15bedb3afe4bfc92a066
Nick Robinson <nrobinson@squareup.com>
2020-01-17 09:13:02 -0500

    initial test

---
+++ tests/acceptance/bands-test.js
@@ -0,0 +1,19 @@
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+
+module('Acceptance | Bands', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('List bands', async function(assert) {
+    this.server.create('band', { name: 'Radiohead' });
+    this.server.create('band', { name: 'Long Distance Calling' });
+    await visit('/');
+    
+    let bandLinks = document.querySelectorAll('.rr-band-link');
+    assert.equal(bandLinks.length, 2, 'All band links are rendered');
+    assert.ok(bandLinks[1].textContent.includes('Long Distance Calling'), 'The other band link contains the band name');
+  });
+});
