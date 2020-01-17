9fb9edb850c6dc7423cf82b45eacb19d0c4916f0
Nick Robinson <nrobinson@squareup.com>
2020-01-17 08:05:34 -0500

    advanced routing

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,8 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  router: service(),
+
   model(params) {
     let bands = this.modelFor('bands')
     return bands.find(band => band.slug === params.slug);
-  }
+  },
 });
